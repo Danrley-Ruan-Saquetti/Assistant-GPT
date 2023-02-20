f2bd8d0da00abe0b8267b5418ddb0e942059aca9
Dan Ruan <danrsaquetti@gmail.com>
2023-02-20 15:11:49 -0300

    update settings

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,8 +7,23 @@ const btSettingsClose = document.getElementById("bt-settings-close")
 const btSettingsReset = document.getElementById("bt-settings-reset")
 const btSettingsSave = document.getElementById("bt-settings-save")
 const panelSettings = document.getElementById("panel-settings")
+const boxLengthLimitHistory = document.getElementById("input-box-length-limit-history")
 
-const OPEN_AI_API_KEY = "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"
+const MAP_SETTINGS_ELEMENTS = {
+    inputKey: document.getElementById("input-key"),
+    inputLimitHistory: document.getElementById("input-limit-history"),
+    inputLengthLimitHistory: document.getElementById("input-length-limit-history"),
+}
+
+const MAP_SETTINGS = {
+    apiKey: null,
+    history: {
+        isLimit: false,
+        limit: 0,
+    }
+}
+
+const API_KEY = "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"
 
 const requestApi = async(body = "") => {
     const response = await fetch("https://api.openai.com/v1/completions", {
@@ -16,7 +31,7 @@ const requestApi = async(body = "") => {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
-            Authorization: "Bearer " + OPEN_AI_API_KEY,
+            Authorization: "Bearer " + MAP_SETTINGS.apiKey,
         },
         body: JSON.stringify({
             model: "text-davinci-003",
@@ -89,24 +104,23 @@ const enableInputQuestion = () => {
 
 const getLocalStorageSettings = (key = "") => {
     try {
-        return JSON.parse(localStorage.getItem(key))
+        return JSON.parse(localStorage.getItem(key)).value
     } catch (err) {
         return null
     }
 }
 
-const removeLocalStorageSettings = (key = "", clear = false) => {
+const removeLocalStorageSettings = ({ key = "", clear = false }) => {
     try {
         !clear ? localStorage.removeItem(key) : localStorage.clear()
-        localStorage.setItem(key, JSON.stringify(value))
     } catch (err) {
         console.log(err)
     }
 }
 
-const saveLocalStorageSettings = (key = "", value = {}) => {
+const saveLocalStorageSettings = ({ key = "", value = {} }) => {
     try {
-        localStorage.setItem(key, JSON.stringify(value))
+        localStorage.setItem(key, JSON.stringify({ value }))
     } catch (err) {
         console.log(err)
     }
@@ -119,6 +133,12 @@ async function sendQuestion() {
     divMain.classList.add("block-question")
 
     addElement(chat, divMain)
+
+    if (MAP_SETTINGS.history.isLimit) {
+        if (document.querySelectorAll(".block-question").length > MAP_SETTINGS.history.limit) {
+            document.querySelectorAll(".block-question")[0].remove()
+        }
+    }
 
     const elQuestion = writeQuestion(question)
 
@@ -154,24 +174,75 @@ const toggleSettings = (value) => {
     panel.classList.toggle("focus-out", value)
 }
 
+const openSettings = () => {
+    MAP_SETTINGS_ELEMENTS.inputKey.value = MAP_SETTINGS.apiKey
+    MAP_SETTINGS_ELEMENTS.inputLimitHistory.checked = MAP_SETTINGS.history.isLimit
+    MAP_SETTINGS_ELEMENTS.inputLengthLimitHistory.value = MAP_SETTINGS.history.limit
+
+    toggleSettingLimitHistory(MAP_SETTINGS.history.isLimit)
+    toggleSettings(true)
+}
+
+const closeSettings = () => {
+    toggleSettings(false)
+}
+
+const saveSettings = () => {
+    MAP_SETTINGS.apiKey = MAP_SETTINGS_ELEMENTS.inputKey.value
+    MAP_SETTINGS.history.isLimit = MAP_SETTINGS_ELEMENTS.inputLimitHistory.checked
+    MAP_SETTINGS.history.limit = MAP_SETTINGS_ELEMENTS.inputLimitHistory.checked ? Number(MAP_SETTINGS_ELEMENTS.inputLengthLimitHistory.value) : 0
+
+    saveLocalStorageSettings({key: "settings.key", value: MAP_SETTINGS.apiKey})
+    saveLocalStorageSettings({key: "settings.history.isLimit", value: MAP_SETTINGS.history.isLimit})
+    saveLocalStorageSettings({key: "settings.history.limit", value: MAP_SETTINGS.history.limit})
+
+    closeSettings()
+}
+
+const resetSettings = () => {
+    removeLocalStorageSettings({clear: true})
+
+    MAP_SETTINGS.apiKey = null
+    MAP_SETTINGS.history.isLimit = false
+    MAP_SETTINGS.history.limit = 0
+
+    MAP_SETTINGS_ELEMENTS.inputKey.value = null
+}
+
+const toggleSettingLimitHistory = (value) => {
+    boxLengthLimitHistory.classList.toggle("active", value)
+
+    if (value) {
+        MAP_SETTINGS_ELEMENTS.inputLengthLimitHistory.value = 1   
+    }
+}
+
 window.onload = () => {
+    MAP_SETTINGS.apiKey = getLocalStorageSettings("settings.key")
+    MAP_SETTINGS.history.isLimit = getLocalStorageSettings("settings.history.isLimit")
+    MAP_SETTINGS.history.limit = getLocalStorageSettings("settings.history.limit")
+    
     inputQuestion.addEventListener("keypress", (e) => {
         if (inputQuestion.value && e.key === "Enter") {sendQuestion()}
     })
 
     btSettingsOpen.addEventListener("click", () => {
-        toggleSettings(true)
+        openSettings()
     })
 
     btSettingsClose.addEventListener("click", () => {
-        toggleSettings(false)
+        closeSettings()
     })
 
     btSettingsReset.addEventListener("click", () => {
-        
+        resetSettings()
     })
 
     btSettingsSave.addEventListener("click", () => {
-        toggleSettings(false)
+        saveSettings()
+    })
+
+    MAP_SETTINGS_ELEMENTS.inputLimitHistory.addEventListener("change", ({target: {checked}}) => {
+        toggleSettingLimitHistory(checked)
     })
 }
