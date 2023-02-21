87ddd1b5888d3e148c969ee1aa01499327a09fae
Dan Ruan <danrsaquetti@gmail.com>
2023-02-20 22:54:09 -0300

    update files

--- src/browser-window.js
+++ src/browser-window.js
@@ -2,12 +2,15 @@ const { BrowserWindow } = require('electron')
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 500,
-        height: 550,
+        width: 750,
+        height: 500,
         title: "Chat GPT",
         darkTheme: true,
-        icon: __dirname + "/icons/icon-open-ai.png"
+        icon: __dirname + "/icons/icon-open-ai.png",
+        show: false,
     })
+
+    win.hide()
 
     win.setMenu(null)
 
