f01aaf77551aa2f1cd8e734fd9aedd6a89bb034b
Dan Ruan <danrsaquetti@gmail.com>
2023-02-21 11:24:35 -0300

    update window

--- src/app.js
+++ src/app.js
@@ -2,37 +2,8 @@ const { app, Menu } = require('electron')
 
 const App = () => {
     const win = require("./browser-window.js")
-    const tray = require("./tray.js")
 
     app.setAppUserModelId(process.execPath)
-
-    const contextMenu = Menu.buildFromTemplate([{
-        label: 'Close',
-        type: 'normal',
-        click: () => {
-            app.exit()
-        }
-    }, ])
-
-    tray.setContextMenu(contextMenu)
-
-    tray.on("click", () => {
-        if (win.isVisible()) {
-            win.hide()
-        } else {
-            win.show()
-        }
-    })
-
-    win.on('minimize', () => {
-        win.hide()
-    })
-
-    win.on('close', (ev) => {
-        ev.preventDefault()
-
-        win.hide()
-    })
 }
 
 app.on('window-all-closed', () => {
--- src/browser-window.js
+++ src/browser-window.js
@@ -1,16 +1,23 @@
 const { BrowserWindow } = require('electron')
 
+const win2 = new BrowserWindow({ width: 0, height: 0, show: false })
+
+win2.setFullScreen(true)
+
+const { height, width } = win2.getBounds()
+
+win2.close()
+
 function createWindow() {
+    const boundsInitial = { width: 600, height: 400 }
     const win = new BrowserWindow({
-        width: 750,
-        height: 500,
+        ...boundsInitial,
         title: "Chat GPT",
         darkTheme: true,
         icon: __dirname + "/icons/icon-open-ai.png",
-        show: false,
     })
 
-    win.hide()
+    win.setPosition(width - boundsInitial.width + 5, height - boundsInitial.height - 35)
 
     win.setMenu(null)
 
